3d9fe1394d73f7ae7580b44110ebd13aad0d98b3
Your Name Heremkdir <jsuika2@fullsail.edu>
2014-01-22 22:00:55 -0500

    add the variables for the condition, and explained what each VAR represents.

--- Conditionals_Industry/JS/script.js
+++ Conditionals_Industry/JS/script.js
@@ -1,8 +1,3 @@
 //created by Jeremy Suika on jan 22 2014
 
 
-
-var kidHeight =47;
-
-
-console.log(kidHeight);
--- Conditionals_Wacky/JS/script.js
+++ Conditionals_Wacky/JS/script.js
@@ -1,1 +1,13 @@
 //created by Jeremy Suika on jan 22 2014
+
+
+// Condition: If Carter reads three or more books he can watch a movie + he is not fussy boy
+
+//          but if he cleans his room he can still watch that movie
+
+
+var booksRead=prompt("How many books did Carter read today?","");     //This variable is to show exactly how many books Carter read, the answer will be in a prompt
+var minBooksRead=3;                                                   // This variable represents the # of books Carter must read, its 3, and does not change.
+
+var fussyBoy=prompt("Was Carter fussy today? (yes or no)","");        //If he is "fussy"? which will be asked in a yes or no Prompt. Anything other then "NO" will result in a FALSE statement in the conditional part.
+var cleanUp=prompt("Did Carter clean up his room? (yes or no)","");   //If he cleans up his room, which will be asked in a yes or no Prompt.
