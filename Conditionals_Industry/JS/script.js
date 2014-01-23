9a250cd1500a8712548a1b367ca1a38fc50a10b9
Your Name Heremkdir <jsuika2@fullsail.edu>
2014-01-23 12:22:24 -0500

    test the first two variables, works fine.

--- Conditionals_Industry/JS/script.js
+++ Conditionals_Industry/JS/script.js
@@ -1,3 +1,24 @@
-//created by Jeremy Suika on jan 22 2014
+//created by Jeremy Suika on jan 23 2014
 
 
+// conditionals_industry
+
+
+
+//I don't know the ins and outs of the industry, and the terms, I am using a example of buying a second moniter for displaying coding better.
+                    // Someone told me to save up for one, makes coding easier.
+
+// I am doing tenary operators for this conditional operation-
+
+//"If I make enough money from freelance jobs and don't go over my monthly expenses, I can get the second moniter for $1200.00
+
+alert("If I make enough money from freelance jobs building websites, and don't go over my monthy expenses I can get a $1,200 moniter");
+
+var job=prompt("How much money did you earn doing freelance jobs for people?  (do not put $)");  //Income make from freelance internet jobs
+
+var expen=prompt("How much was your bills this month?  (do not put$)");  //expenses for the month
+
+
+
+
+console.log(("$")+(job-expen));
