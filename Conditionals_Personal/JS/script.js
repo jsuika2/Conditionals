dc523b596ce68802ea3c68d691e106a7e855b7d9
Your Name Heremkdir <jsuika2@fullsail.edu>
2014-01-23 10:02:59 -0500

    remove the  on the BONUS and EACHCAR, as it does not compute in the concole log.

--- Conditionals_Personal/JS/script.js
+++ Conditionals_Personal/JS/script.js
@@ -3,15 +3,17 @@
 
 //If I sell 10 plus cars I get a $1,000 bonus. So the bonus is conditional on PROMPTS being > 10 cars.
 
-var bonus
-
 
 var amountCarsSold=prompt("How many cars did you sell this month","");
 
-var bonus="$"+1000;
+var bonus=1000;
 
-var eachCar="$"+100;
+var eachCar=100;
 
-if(carsSold > 10){
+
+
+if(amountCarsSold > 10){
     console.log(bonus);
+}else{
+    console.log("No bonus, only "+eachCar * amountCarsSold);
 }
