d8e9ca827c836939598c3829f71d5e2354d23b46
Your Name Heremkdir <jsuika2@fullsail.edu>
2014-01-23 09:52:21 -0500

    create Personal Condition formula, and set up intial variables, and prompts.

--- Conditionals_Personal/JS/script.js
+++ Conditionals_Personal/JS/script.js
@@ -1,1 +1,17 @@
-//created by Jeremy Suika on jan 22 2014
+//created by Jeremy Suika on jan 23 2014
+//conditionals_personal
+
+//If I sell 10 plus cars I get a $1,000 bonus. So the bonus is conditional on PROMPTS being > 10 cars.
+
+var bonus
+
+
+var amountCarsSold=prompt("How many cars did you sell this month","");
+
+var bonus="$"+1000;
+
+var eachCar="$"+100;
+
+if(carsSold > 10){
+    console.log(bonus);
+}
